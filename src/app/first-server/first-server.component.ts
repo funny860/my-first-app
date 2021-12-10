ed15cd41df2dc926a47faec133dab996af94cb9c
funny860 <phanindrarock@gmail.com>
2021-12-10 11:37:22 +0530

    commit on 'master'

---
+++ src/app/first-server/first-server.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+//add a decorator
+@Component({
+  selector: 'app-first-server',
+  templateUrl: './first-server.component.ts',
+})
+export class firstServerComponent {}
